db73471afab136b168e0065946da576e51831a5a
AQP20171679 <alejandraadriantejada@gmail.com>
2017-09-25 17:02:48 -0500

    video grande

--- app.js
+++ app.js
@@ -3,74 +3,72 @@
 const API_KEY = "AIzaSyAlPKe5Id3BSVxyfKZT-eqnZZr2pJRE6zQ";
 
 let app = {
-   result: {
-      videos: [],
-      selectedVideo: null,
-      searchTerm: "iPhone X"
-   },
+      result: {
+            videos: [],
+            selectedVideo: null,
+            searchTerm: "iPhone X"
+      },
 
-   init: function() {
-      //app.videoSearch("iPhone");
-      app.youtubeSearch("iPhoneX");
-      app.setup();
-   },
-   
-   setup: function () {
-      $('#search').keyup(function(e) {
-            if(e.which == 13) {
-                  $("#root").empty();
-                  let searchHere = $('#search').val();
-                  console.log(searchHere);
-                  app.youtubeSearch($('#search').val());
-            }
-      });
-   },
-   //<iframe className="embed-responsive-item" src={url}> </iframe>
-   getVideoList: function(videos) {
-      return videos.map((video, index) => {
-            var title = video.snippet.title;
-            // var description = video.entry.media$group.media$description.$t;
-            // var viewcount = video.entry.yt$statistics.viewCount;
-            // var author = video.entry.author[0].name.$t;
-            const imageUrl = video.snippet.thumbnails.default.url;
+      init: function() {
+            //app.videoSearch("iPhone");
+            app.youtubeSearch("iPhoneX");
+            app.setup();
+      },
+      
+      setup: function () {
+            $('#search').keyup(function(e) {
+                  if(e.which == 13) {
+                        $("#root").empty();
+                        $('#video').empty();
+                        // let searchHere = $('#search').val();
+                        // console.log(searchHere);
+                        app.youtubeSearch($('#search').val());
+                  }
+            });
+      },
+      getVideo: function (video){
+            const title = video.snippet.title;
+            const description = video.snippet.description;
             const url = `https://www.youtube.com/embed/${video.id.videoId}`;
-            return `<li> 
-                        <img class="media-object" src=${imageUrl} /> 
-                        <p>${title}</p>
-                        <p> 
-                              <iframe class="embed-responsive-item" src=${url}> </iframe>
-                        </p>
-                  </li>`;
-      });
-   },
-   youtubeSearch: function(searchTerm) {
-      console.log(searchTerm);
+            return `<iframe class="embed-responsive-item" src=${url} height="200" width="300"> </iframe>
+                        <p><b>${title}</b></p>
+                        <p>${description}</p>`;
+      
+      },      
+      //<iframe className="embed-responsive-item" src={url}> </iframe>
+      getVideoList: function(videos) {
+            return videos.map((video, index) => {
+                  const title = video.snippet.title;
+                  const description = video.snippet.description;
+                  const imageUrl = video.snippet.thumbnails.default.url;
+                  return `<li class="list-group-item">
+                              <div class='row'>
+                                    <div class='col-md-6 col-ls-6 col-xs-6 col-sm-6'>
+                                          <img class="media-object" src=${imageUrl} /> 
+                                    </div>
+                                    <div class='col-md-6 col-ls-6 col-xs-6 col-sm-6'>
+                                          <p><b>${title}</b></p>
+                                    </div>
+                              </div>
+                        </li>`;
+            });
+      },
+      youtubeSearch: function(searchTerm) {
+            console.log(searchTerm);
 
-      YTSearch({ key: API_KEY, term: searchTerm }, data => {
-         console.log("result", data);
-         app.result = {
-            videos: data,
-            selectedVideo: data[0],
-            searchTerm: searchTerm
-         };
-         var list = app.getVideoList(app.result.videos);
-         console.log("lis: ", list);
-         $("#root").append(list);
-      });
-   },
-   videoSearch: function(searchTerm) {
-      jQuery.getJSON("list.json", data => {
-         console.log("result", data.items);
-         app.result = {
-            videos: data.items,
-            selectedVideo: data.items[0],
-            searchTerm: searchTerm
-         };
-         var list = app.getVideoList(app.result.videos);
-         console.log("lis: ", list);
-         $("#root").append(list);
-      });
-   }
+            YTSearch({ key: API_KEY, term: searchTerm }, data => {
+            console.log("result", data);
+            app.result = {
+                  videos: data,
+                  selectedVideo: data[0],
+                  searchTerm: searchTerm
+            };
+            var list = app.getVideoList(app.result.videos);
+            let firstVideo = app.getVideo(app.result.selectedVideo);
+            $("#root").append(list);
+            $('#video').append(firstVideo)
+            });
+      }
 };
 
 $(document).ready(function() {
