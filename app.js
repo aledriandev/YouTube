45103eac6844c9e1ef38abbbe19e0b9218dc33ca
AQP20171679 <alejandraadriantejada@gmail.com>
2017-09-26 06:59:27 -0500

    cambando a clases, busca

--- app.js
+++ app.js
@@ -2,50 +2,52 @@
 
 const API_KEY = "AIzaSyAlPKe5Id3BSVxyfKZT-eqnZZr2pJRE6zQ";
 
-let app = {
-      result: {
-            videos: [],
-            selectedVideo: null,
-            searchTerm: "Laboratoria"
-      },
+class App {
+      // result: {
+      constructor () {
+            this.videos= [];
+            this.selectedVideo= null;
+            this.searchTerm= "Laboratoria";
+      }
 
-      init: function() {
-            //app.videoSearch("iPhone");
-            app.youtubeSearch("Laboratoria");
-            app.setup();
-      },
+      init () {
+            //this.videoSearch("iPhone");
+            this.youtubeSearch("Laboratoria");
+            this.setup();
+      }
       
-      setup: function () {
-            $('#search').keyup(function(e) {
+      setup () {
+            $('#search').keyup((e) => {
                   if(e.which == 13) {
-                        app.youtubeSearch($('#search').val());
+                        console.log($('#search').val());
+                        this.youtubeSearch($('#search').val());
                   }
             });
-      },
+      }
 
-      getVideo: function (video){
+      getVideo (video){
             $('#video').empty();
             const url = `http://www.youtube.com/embed/${video.id.videoId}`;
             return `<iframe class="embed-responsive-item" src=${url}> </iframe>`;
-      },
+      }
 
-      getDetails: function (video){
+      getDetails (video){
             $('#details').empty();
             const title = video.snippet.title;
             const description = video.snippet.description;
             return ` <p><b>${title}</b></p>
                         <p>${description}</p>`;
                        
-      }, 
+      } 
       //<iframe className="embed-responsive-item" src={url}> </iframe>
-      getVideoList: function(videos) {
+      getVideoList (videos) {
             $("#root").empty();
             return videos.map((video, index) => {
                   const title = video.snippet.title;
                   const description = video.snippet.description;
                   const imageUrl = video.snippet.thumbnails.default.url;
-                  // app.result.selectedVideo = videos[index];
-                  return `<li class="list-group-item" id=${index} onclick='app.select(this)'>
+                  // this.result.selectedVideo = videos[index];
+                  return `<li class="list-group-item" id=${index} onclick='this.select(this)'>
                               <div class='row'>
                                     <div class='col-md-6 col-ls-6 col-xs-6 col-sm-6'>
                                           <img class="media-object" src=${imageUrl} /> 
@@ -56,30 +58,28 @@ let app = {
                               </div>
                         </li>`;
             });
-      },
-      select: (e) => {
-            let index = parseInt($(e).attr("id"));
-            app.result.selectedVideo = app.result.videos[index];
+      }
+      // select (e) => {
+      //       let index = parseInt($(e).attr("id"));
+      //       this.result.selectedVideo = this.result.videos[index];
 
-            let video = app.getVideo(app.result.selectedVideo);
-            let detail = app.getDetails(app.result.selectedVideo);
-            $('#video').append(video)
-            $('#details').append(detail);
+      //       let video = this.getVideo(this.result.selectedVideo);
+      //       let detail = this.getDetails(this.result.selectedVideo);
+      //       $('#video').thisend(video)
+      //       $('#details').thisend(detail);
             
-      },
-      youtubeSearch: function(searchTerm) {
+      // }
+      youtubeSearch (searchTerm) {
             console.log(searchTerm);
 
             YTSearch({ key: API_KEY, term: searchTerm }, data => {
-            console.log("result", data);
-            app.result = {
-                  videos: data,
-                  selectedVideo: data[0],
-                  searchTerm: searchTerm
-            };
-            var list = app.getVideoList(app.result.videos);
-            let firstVideo = app.getVideo(app.result.selectedVideo);
-            let firsDetail = app.getDetails(app.result.selectedVideo);
+            // console.log("result" data);
+            this.videos= data;
+            this.selectedVideo= data[0];
+            this.searchTerm= searchTerm;
+            var list = this.getVideoList(this.videos);
+            let firstVideo = this.getVideo(this.selectedVideo);
+            let firsDetail = this.getDetails(this.selectedVideo);
             $("#root").append(list);
             $('#video').append(firstVideo)
             $('#details').append(firsDetail);
@@ -88,7 +88,6 @@ let app = {
 };
 
 $(document).ready(function() {
+      var app = new App ();
       app.init();
-      
-
 });
